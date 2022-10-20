3f01e1681092c10221425929b45d5658f584f7a5
kimdoeun <kdeun1@ex-em.com>
2022-10-20 22:42:03 +0900

    1. 리액트 컴포넌트 기초

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import Expenses from "./components/Expenses";
+// import React from 'react';
+import Expenses from "./components/Expenses/Expenses";
 import "./App.css";
 
-function App() {
+const App = () => {
   const expenses = [
     { id: "a1", title: "새우깡", amount: 1300, date: new Date(2021, 10, 22) },
     { id: "a2", title: "그래놀라", amount: 16800, date: new Date(2022, 1, 21) },
     { id: "a3", title: "별뽀빠이", amount: 12240, date: new Date(2022, 5, 2) },
     { id: "a4", title: "누네띠네", amount: 13620, date: new Date(2023, 3, 11) },
   ];
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses }),
+  // );
   return (
     <div>
       <h2>Let's get started!</h2>
