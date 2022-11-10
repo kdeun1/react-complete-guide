dcebcef2b90ac655b5678f5f0c8d86f216f42eba
kde <kdeun1@naver.com>
2022-11-11 00:48:55 +0900

    5. practice simple project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = ({ name, age }) => {
+    setUsersList(prevUsersList => {
+      return [...prevUsersList, { name, age, id: Math.random().toString() }];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
